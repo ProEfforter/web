91e1cb8c82d246848eea002f4be876d5d5f4a8a5
ProEfforter <ProEfforter@inha.edu>
2023-10-28 17:51:11 +0900

    9weeks_class

--- public/javascripts/firebase.js
+++ public/javascripts/firebase.js
@@ -6,15 +6,15 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  // const firebaseConfig = {
-  //   apiKey: "AIzaSyB059QqtnBPBVpoP9XDWdTE5q-2N-vuT60",
-  //   authDomain: "fir-a9d4d.firebaseapp.com",
-  //   projectId: "fir-a9d4d",
-  //   storageBucket: "fir-a9d4d.appspot.com",
-  //   messagingSenderId: "118041568771",
-  //   appId: "1:118041568771:web:dbe1dba3ca50956e0f1036",
-  //   measurementId: "G-JDHMXLDMFK"
-  // };
+  const firebaseConfig = {
+    apiKey: "AIzaSyB059QqtnBPBVpoP9XDWdTE5q-2N-vuT60",
+    authDomain: "fir-a9d4d.firebaseapp.com",
+    projectId: "fir-a9d4d",
+    storageBucket: "fir-a9d4d.appspot.com",
+    messagingSenderId: "118041568771",
+    appId: "1:118041568771:web:dbe1dba3ca50956e0f1036",
+    measurementId: "G-JDHMXLDMFK"
+  };
 
   // Initialize Firebase
   export const app = initializeApp(firebaseConfig);
--- routes/locals.js
+++ routes/locals.js
@@ -5,5 +5,8 @@ router.get('/', function(req, res, next) {
   // 서버가 오픈하는 경로가 /일때 index.ejs를 불러들여 렌더링 한다.
   res.render('index', { title: '지역검색', pageName:'locals/search.ejs'});
 });
-
+// 즐겨찾기 페이지 이동
+router.get('/favorite', function(req, res) {
+  res.render('index', { title: '즐겨찾기', pageName:'locals/favorite.ejs'});
+});
 module.exports = router;
