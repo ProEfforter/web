57c3cd51c5a93c8de6069e8146ad14d63ab90253
ProEfforter <ProEfforter@inha.edu>
2023-11-11 17:58:48 +0900

    11weeks_class

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* 게시글 페이지 */
-router.get('/', function(req, res, next) {
-  res.render('posts', { title: '게시글' });
+router.get('/', function(req, res) {
+  res.render('index', { title: '게시판', pageName :'posts/list.ejs' });
+});
+
+router.get('/insert', function(req, res) {
+  res.render('index', { title: '글쓰기', pageName :'posts/insert.ejs' });
 });
 
 module.exports = router;
